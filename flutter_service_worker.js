b4445a454b9e52c71499ccd22e5cebe9c16a83b9
dtubegp <138803070+dtubego@users.noreply.github.com>
2023-07-07 17:39:47 +0200

    Fixed ads as a web app.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -65,8 +65,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "93219a5e1db4fc557d0a0666755c5a49
 "flutter.js": "6b515e434cea20006b3ef1726d2c8894",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "a95788ea90b4dea3bacfaed6f5066bb5",
-"/": "a95788ea90b4dea3bacfaed6f5066bb5",
+"index.html": "bb8902bfabeaf663caea0418ff2d18cf",
+"/": "bb8902bfabeaf663caea0418ff2d18cf",
 "main.dart.js": "873dc642d691e9cc061fb5518668868c",
 "manifest.json": "82eac82cd648feb10eaff83e19f7cec9",
 "version.json": "ff8a32fb44bbc0cd5b11932ca0b16fb4"};
